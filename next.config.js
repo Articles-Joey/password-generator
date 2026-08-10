a1b1a279f3235633f8593cee5b8c4db9368e1f45
ArticlesJoey <joey@articles.media>
2026-08-10 03:55:28 -0400

    small config tweaks

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async redirects() {
-    return [
-      // Basic redirect
-      {
-        source: '/menus',
-        destination: '/the-restaurant/menus',
-        permanent: true,
-      },
-      {
-        source: '/the-patio',
-        destination: '/the-restaurant/the-patio',
-        permanent: true,
-      },
-    ]
-  },
+  poweredByHeader: false,
 }
 
 module.exports = nextConfig
